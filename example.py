d2278221477eac014f85f6e56687df5998affaf9
Blaise <blaise.lengrand@gmail.com>
2019-07-03 23:49:13 +0200

    Fixing repo name

--- example.py
+++ example.py
@@ -9,7 +9,10 @@ import shlex
 import subprocess
 import zlib
 
-## git-lego dep "https://github.com/blaizard/git-lego.git" "loader.py" "master"
+## git-lego dep "https://github.com/blaizard/git-lego.git" "loader.py" "master" 1906219065
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
 import os
 import subprocess
 import sys
@@ -18,12 +21,15 @@ def gitLegoLoader(command = None):
 	if len(sys.argv) < 2 or sys.argv[1] != "git-lego": return
 	gitLegoPath = os.path.join(os.path.realpath(os.path.expanduser("~") if os.path.expanduser("~") else os.path.dirname(__file__)), ".git-lego")
 	if not os.path.isdir(gitLegoPath): os.mkdir(gitLegoPath)
-	gitLegoDepPath = os.path.join(gitLegoPath, "https.github.com.blaizard.git-lego.git")
+	gitLegoDepPath = os.path.join(gitLegoPath, "https.github.com.blaizard.git.lego.git")
 	if not os.path.isdir(gitLegoDepPath): subprocess.call(["git", "clone", "https://github.com/blaizard/git-lego.git", gitLegoDepPath])
 	lib = imp.load_module("lib", None, os.path.join(gitLegoDepPath, "lib"), ('', '', imp.PKG_DIRECTORY))
 	gitlego = lib.interface.Interface(__file__, cwd=gitLegoPath)
 	sys.exit(gitlego.run(command) if command else gitlego.run(sys.argv[2:]))
+
 ## git-lego end
+
+
 
 ## git-lego dep "https://github.com/blaizard/irapp.git" ".irapp/commands.py" "master" 2882168141
 #!/usr/bin/python
@@ -50,6 +56,8 @@ class Commands:
 		time.sleep(float(argList[0]))
 
 ## git-lego end
+
+
 
 
 
@@ -81,6 +89,8 @@ class Commands:
 		time.sleep(float(argList[0]))
 
 ## git-lego end
+
+
 
 
 
--- loader.py
+++ loader.py
@@ -9,7 +9,7 @@ def gitLegoLoader(command = None):
 	if len(sys.argv) < 2 or sys.argv[1] != "git-lego": return
 	gitLegoPath = os.path.join(os.path.realpath(os.path.expanduser("~") if os.path.expanduser("~") else os.path.dirname(__file__)), ".git-lego")
 	if not os.path.isdir(gitLegoPath): os.mkdir(gitLegoPath)
-	gitLegoDepPath = os.path.join(gitLegoPath, "https.github.com.blaizard.git-lego.git")
+	gitLegoDepPath = os.path.join(gitLegoPath, "https.github.com.blaizard.git.lego.git")
 	if not os.path.isdir(gitLegoDepPath): subprocess.call(["git", "clone", "https://github.com/blaizard/git-lego.git", gitLegoDepPath])
 	lib = imp.load_module("lib", None, os.path.join(gitLegoDepPath, "lib"), ('', '', imp.PKG_DIRECTORY))
 	gitlego = lib.interface.Interface(__file__, cwd=gitLegoPath)
